679a21384f39572e658641a5d632035092366681
alex <alex@nabu.be>
2016-09-20 10:11:59 +0200

    Updated to tokens

--- src/main/java/be/nabu/libs/services/cache/ServiceRefresher.java
+++ src/main/java/be/nabu/libs/services/cache/ServiceRefresher.java
@@ -1,11 +1,11 @@
 package be.nabu.libs.services.cache;
 
 import java.io.IOException;
-import java.security.Principal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import be.nabu.libs.authentication.api.Token;
 import be.nabu.libs.cache.api.CacheRefresher;
 import be.nabu.libs.services.ServiceRuntime;
 import be.nabu.libs.services.api.DefinedService;
@@ -18,18 +18,18 @@ public class ServiceRefresher implements CacheRefresher {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	private DefinedService service;
 	private ExecutionContextProvider contextProvider;
-	private Principal principal;
+	private Token token;
 
-	public ServiceRefresher(ExecutionContextProvider contextProvider, Principal principal, DefinedService service) {
+	public ServiceRefresher(ExecutionContextProvider contextProvider, Token token, DefinedService service) {
 		this.contextProvider = contextProvider;
-		this.principal = principal;
+		this.token = token;
 		this.service = service;
 	}
 	
 	@Override
 	public Object refresh(Object key) throws IOException {
 		if (key instanceof ComplexContent) {
-			ServiceRuntime runtime = new ServiceRuntime(service, contextProvider.newExecutionContext(principal));
+			ServiceRuntime runtime = new ServiceRuntime(service, contextProvider.newExecutionContext(token));
 			// disable caching for the refresh
 			runtime.setAllowCaching(false);
 			try {
